7a1c221b3036dd12b1f1e69484d9bdf8e4b7b31c
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2023-05-08 10:47:20 +0200

    Add files via upload

---
+++ AplikacijaZaRestoran/Juha.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikacijaZaRestoran
+{
+    public partial class Juha : Form
+    {
+
+        Pocetna pocetna;
+        public Juha(Pocetna poc)
+        {
+            InitializeComponent();
+            this.pocetna = poc;
+         
+        }
+
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        public void pilecaJuha_Click(object sender, EventArgs e)
+        {
+            object[] row = new object[] { "Pileća juha", "1", "4.00" };
+
+            pocetna.AddRowToDataGridView(row);
+        }
+
+        private void povrtnaJuha_Click(object sender, EventArgs e)
+        {
+            object[] row = new object[] { "Povrtna juha", "1", "3.50" };
+
+            pocetna.AddRowToDataGridView(row);
+        }
+
+        private void ribljaJuha_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void gljiveJuha_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void rajcicaJuha_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ AplikacijaZaRestoran/Salata.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace AplikacijaZaRestoran
+{
+    partial class Salata
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paprika = new System.Windows.Forms.Button();
+            this.krastavci = new System.Windows.Forms.Button();
+            this.limun = new System.Windows.Forms.Button();
+            this.rajcica = new System.Windows.Forms.Button();
+            this.grckaSalata = new System.Windows.Forms.Button();
+            this.sopskaSalata = new System.Windows.Forms.Button();
+            this.meksickaSalata = new System.Windows.Forms.Button();
+            this.cezarSalata = new System.Windows.Forms.Button();
+            this.francuskaSalata = new System.Windows.Forms.Button();
+            this.mijesanaSalata = new System.Windows.Forms.Button();
+            this.kupusSalata = new System.Windows.Forms.Button();
+            this.zelenaSalata = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // paprika
+            // 
+            this.paprika.BackColor = System.Drawing.Color.Violet;
+            this.paprika.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.paprika.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.paprika.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.paprika.ForeColor = System.Drawing.Color.Purple;
+            this.paprika.Location = new System.Drawing.Point(468, 270);
+            this.paprika.Margin = new System.Windows.Forms.Padding(4);
+            this.paprika.Name = "paprika";
+            this.paprika.Size = new System.Drawing.Size(124, 98);
+            this.paprika.TabIndex = 27;
+            this.paprika.Text = "Paprika";
+            this.paprika.UseVisualStyleBackColor = false;
+            // 
+            // krastavci
+            // 
+            this.krastavci.BackColor = System.Drawing.Color.Violet;
+            this.krastavci.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.krastavci.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.krastavci.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.krastavci.ForeColor = System.Drawing.Color.Purple;
+            this.krastavci.Location = new System.Drawing.Point(319, 270);
+            this.krastavci.Margin = new System.Windows.Forms.Padding(4);
+            this.krastavci.Name = "krastavci";
+            this.krastavci.Size = new System.Drawing.Size(124, 98);
+            this.krastavci.TabIndex = 26;
+            this.krastavci.Text = "Krastavci";
+            this.krastavci.UseVisualStyleBackColor = false;
+            this.krastavci.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // limun
+            // 
+            this.limun.BackColor = System.Drawing.Color.Violet;
+            this.limun.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.limun.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.limun.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.limun.ForeColor = System.Drawing.Color.Purple;
+            this.limun.Location = new System.Drawing.Point(15, 270);
+            this.limun.Margin = new System.Windows.Forms.Padding(4);
+            this.limun.Name = "limun";
+            this.limun.Size = new System.Drawing.Size(124, 98);
+            this.limun.TabIndex = 25;
+            this.limun.Text = "Limun";
+            this.limun.UseVisualStyleBackColor = false;
+            // 
+            // rajcica
+            // 
+            this.rajcica.BackColor = System.Drawing.Color.Violet;
+            this.rajcica.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rajcica.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.rajcica.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.rajcica.ForeColor = System.Drawing.Color.Purple;
+            this.rajcica.Location = new System.Drawing.Point(167, 270);
+            this.rajcica.Margin = new System.Windows.Forms.Padding(4);
+            this.rajcica.Name = "rajcica";
+            this.rajcica.Size = new System.Drawing.Size(124, 98);
+            this.rajcica.TabIndex = 24;
+            this.rajcica.Text = "Rajčica";
+            this.rajcica.UseVisualStyleBackColor = false;
+            // 
+            // grckaSalata
+            // 
+            this.grckaSalata.BackColor = System.Drawing.Color.Violet;
+            this.grckaSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.grckaSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.grckaSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.grckaSalata.ForeColor = System.Drawing.Color.Purple;
+            this.grckaSalata.Location = new System.Drawing.Point(468, 146);
+            this.grckaSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.grckaSalata.Name = "grckaSalata";
+            this.grckaSalata.Size = new System.Drawing.Size(124, 98);
+            this.grckaSalata.TabIndex = 23;
+            this.grckaSalata.Text = "Grčka salata";
+            this.grckaSalata.UseVisualStyleBackColor = false;
+            // 
+            // sopskaSalata
+            // 
+            this.sopskaSalata.BackColor = System.Drawing.Color.Violet;
+            this.sopskaSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sopskaSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.sopskaSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.sopskaSalata.ForeColor = System.Drawing.Color.Purple;
+            this.sopskaSalata.Location = new System.Drawing.Point(319, 146);
+            this.sopskaSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.sopskaSalata.Name = "sopskaSalata";
+            this.sopskaSalata.Size = new System.Drawing.Size(124, 98);
+            this.sopskaSalata.TabIndex = 22;
+            this.sopskaSalata.Text = "Šopska salata";
+            this.sopskaSalata.UseVisualStyleBackColor = false;
+            // 
+            // meksickaSalata
+            // 
+            this.meksickaSalata.BackColor = System.Drawing.Color.Violet;
+            this.meksickaSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.meksickaSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.meksickaSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.meksickaSalata.ForeColor = System.Drawing.Color.Purple;
+            this.meksickaSalata.Location = new System.Drawing.Point(167, 146);
+            this.meksickaSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.meksickaSalata.Name = "meksickaSalata";
+            this.meksickaSalata.Size = new System.Drawing.Size(124, 98);
+            this.meksickaSalata.TabIndex = 21;
+            this.meksickaSalata.Text = "Meksička salata";
+            this.meksickaSalata.UseVisualStyleBackColor = false;
+            // 
+            // cezarSalata
+            // 
+            this.cezarSalata.BackColor = System.Drawing.Color.Violet;
+            this.cezarSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cezarSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cezarSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.cezarSalata.ForeColor = System.Drawing.Color.Purple;
+            this.cezarSalata.Location = new System.Drawing.Point(15, 146);
+            this.cezarSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.cezarSalata.Name = "cezarSalata";
+            this.cezarSalata.Size = new System.Drawing.Size(124, 98);
+            this.cezarSalata.TabIndex = 20;
+            this.cezarSalata.Text = "Cezar salata";
+            this.cezarSalata.UseVisualStyleBackColor = false;
+            // 
+            // francuskaSalata
+            // 
+            this.francuskaSalata.BackColor = System.Drawing.Color.Violet;
+            this.francuskaSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.francuskaSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.francuskaSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.francuskaSalata.ForeColor = System.Drawing.Color.Purple;
+            this.francuskaSalata.Location = new System.Drawing.Point(468, 27);
+            this.francuskaSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.francuskaSalata.Name = "francuskaSalata";
+            this.francuskaSalata.Size = new System.Drawing.Size(124, 98);
+            this.francuskaSalata.TabIndex = 19;
+            this.francuskaSalata.Text = "Francuska salata ";
+            this.francuskaSalata.UseVisualStyleBackColor = false;
+            // 
+            // mijesanaSalata
+            // 
+            this.mijesanaSalata.BackColor = System.Drawing.Color.Violet;
+            this.mijesanaSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mijesanaSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.mijesanaSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.mijesanaSalata.ForeColor = System.Drawing.Color.Purple;
+            this.mijesanaSalata.Location = new System.Drawing.Point(319, 27);
+            this.mijesanaSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.mijesanaSalata.Name = "mijesanaSalata";
+            this.mijesanaSalata.Size = new System.Drawing.Size(124, 98);
+            this.mijesanaSalata.TabIndex = 18;
+            this.mijesanaSalata.Text = "Miješana salata ";
+            this.mijesanaSalata.UseVisualStyleBackColor = false;
+            // 
+            // kupusSalata
+            // 
+            this.kupusSalata.BackColor = System.Drawing.Color.Violet;
+            this.kupusSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kupusSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.kupusSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.kupusSalata.ForeColor = System.Drawing.Color.Purple;
+            this.kupusSalata.Location = new System.Drawing.Point(167, 27);
+            this.kupusSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.kupusSalata.Name = "kupusSalata";
+            this.kupusSalata.Size = new System.Drawing.Size(124, 98);
+            this.kupusSalata.TabIndex = 17;
+            this.kupusSalata.Text = "Kupus salata";
+            this.kupusSalata.UseVisualStyleBackColor = false;
+            // 
+            // zelenaSalata
+            // 
+            this.zelenaSalata.BackColor = System.Drawing.Color.Violet;
+            this.zelenaSalata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.zelenaSalata.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.zelenaSalata.Font = new System.Drawing.Font("Elephant", 11.8F);
+            this.zelenaSalata.ForeColor = System.Drawing.Color.Purple;
+            this.zelenaSalata.Location = new System.Drawing.Point(15, 27);
+            this.zelenaSalata.Margin = new System.Windows.Forms.Padding(4);
+            this.zelenaSalata.Name = "zelenaSalata";
+            this.zelenaSalata.Size = new System.Drawing.Size(124, 98);
+            this.zelenaSalata.TabIndex = 16;
+            this.zelenaSalata.Text = "Zelena salata";
+            this.zelenaSalata.UseVisualStyleBackColor = false;
+            // 
+            // Salata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(605, 518);
+            this.Controls.Add(this.paprika);
+            this.Controls.Add(this.krastavci);
+            this.Controls.Add(this.limun);
+            this.Controls.Add(this.rajcica);
+            this.Controls.Add(this.grckaSalata);
+            this.Controls.Add(this.sopskaSalata);
+            this.Controls.Add(this.meksickaSalata);
+            this.Controls.Add(this.cezarSalata);
+            this.Controls.Add(this.francuskaSalata);
+            this.Controls.Add(this.mijesanaSalata);
+            this.Controls.Add(this.kupusSalata);
+            this.Controls.Add(this.zelenaSalata);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Salata";
+            this.Text = "Salata";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button paprika;
+        private System.Windows.Forms.Button krastavci;
+        private System.Windows.Forms.Button limun;
+        private System.Windows.Forms.Button rajcica;
+        private System.Windows.Forms.Button grckaSalata;
+        private System.Windows.Forms.Button sopskaSalata;
+        private System.Windows.Forms.Button meksickaSalata;
+        private System.Windows.Forms.Button cezarSalata;
+        private System.Windows.Forms.Button francuskaSalata;
+        private System.Windows.Forms.Button mijesanaSalata;
+        private System.Windows.Forms.Button kupusSalata;
+        private System.Windows.Forms.Button zelenaSalata;
+    }
+}
